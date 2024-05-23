dfbe1be45a5b393d2e4d62c33205e8dcbbe77816
My Name <someone@email.com>
2024-05-23 11:58:08 +0500

    portfolio

--- containers/Education.tsx
+++ containers/Education.tsx
@@ -17,7 +17,7 @@ const Education = () => {
                 </div>
               </div>
               <div className="pl-4">
-                <h4 className="display-3 text-dark">Education</h4>
+                <h4 className="display-3 text-white">Education</h4>
               </div>
             </div>
             <Row className="row-grid align-items-center">
@@ -31,7 +31,7 @@ const Education = () => {
             </Row>
           </Container>
           <div className="separator separator-bottom separator-skew zindex-100">
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="none"
               version="1.1"
@@ -39,8 +39,8 @@ const Education = () => {
               x="0"
               y="0"
             >
-              <polygon className="fill-white" points="2560 0 2560 100 0 100" />
-            </svg>
+              <polygon className="blue" points="2560 0 2560 100 0 100" />
+            </svg> */}
           </div>
         </section>
       </Fade>
