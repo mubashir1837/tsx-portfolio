505346dc93f7f118ebca52e831f08cad39f1390e
My Name <someone@email.com>
2024-06-01 03:46:44 +0500

    website

--- components/AboutMeCard.tsx
+++ components/AboutMeCard.tsx
@@ -1,12 +1,13 @@
+// components/AboutMeCard.tsx
 import React from 'react';
-import { Card, CardBody } from 'reactstrap';
+import { Card, CardBody, Badge } from 'reactstrap';
 import Image from 'next/image';
 import { AboutMeType } from '../types/sections';
 
 const AboutMeCard: React.FC<AboutMeType> = ({ title, description, image, bullets }) => {
   return (
-    <Card className="card-width shadow-lg--hover shadow mt-4 bg-light">
-      <CardBody>
+    <Card className="card-width shadow-lg--hover shadow mt-4 w-10 bg-light ">
+      <CardBody className='w-10'>
         <div className="d-flex px-3">
           <div className="pl-4">
             <h5 className="text-dark">{title}</h5>
